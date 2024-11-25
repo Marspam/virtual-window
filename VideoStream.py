55aa2c138f961e4894fe1a3f97376b1d94a34075
Marspam <78793070+Marspam@users.noreply.github.com>
2024-11-25 20:30:21 +0100

    Add files via upload

--- VideoStream.py
+++ VideoStream.py
@@ -12,8 +12,9 @@ class VideoStream:
     set default to 0 
     """
     def __init__(self, src = 0):  #optional arg for camera source 
+        self.src = src
         #start using camera param 0 for index of cam(if multiple)
-        self.capture = cv2.VideoCapture(src) 
+        self.capture = cv2.VideoCapture(self.src) 
         #only for initialisation in class. otherwise done in update() 
         (self.ret,self.frame) = self.capture.read()  # returned stream boolean and captured frame 
         
@@ -32,15 +33,8 @@ class VideoStream:
                 self.stop()
             else:
                 (self.ret, self.frame) = self.capture.read()
-                
-                #returns only 1 frame, must be in loop  
-                #cv2.imshow("Video",cv2.flip(self.frame,1))
-                #cv2.waitKey(0) 
-                #if cv2.waitKey(100) == ord('q'):
-                #self.stop()
         
     def stop(self):
-        # resolved terminate called without active exception        causing core dump 
         self.stopped = True
         self.capture.release()  # release camera resources
         cv2.destroyAllWindows() # close window
